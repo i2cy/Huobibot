678f52681eef7f2f59631527b05ff038bae83490
i2cy <i2cy@outlook.com>
2021-04-27 15:52:59 +0800

    added codybot.py api for communication and reforming codybot as a cloud service

---
+++ api/codybot.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: i2cy(i2cy@outlook.com)
+# Filename: codybot
+# Created on: 2021/4/27
+
+import json
+from i2cylib.network.I2TCP_protocol.I2TCP_client import *
+
+
+CONFIG = "configs/codybot.json"
+
+
+class CodyAPIClient:
+
+    def __init__(self):
+        pass
